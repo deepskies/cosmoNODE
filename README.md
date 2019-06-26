a65bd0a298b1ac30528a4441490671fbfb7d8a6b
anandijain <anandj@uchicago.edu>
2019-06-26 11:52:14 -0500

    flux over time

--- README.md
+++ README.md
@@ -16,7 +16,10 @@ https://colab.research.google.com/drive/1e9g_X_DRhREfIhSVqwXqPqTriWVPRVZc
 Time-of documentation:
 6/26/19
 
-11:04 trained using anandijain/sip/gym-sip/regression.py on training_set.csv of LSST and tested on the test_set_sample.py
+11:04 trained using anandijain/sip/gym-sip/regression.py neural network on training_set.csv of LSST and tested on the test_set_sample.py
+	
+	+ the model is just linear 5 -> 20 -> 3 -> 8 -> 4 -> 1 (definitely not optimal, but wanted to just train on anything)
+
 	+ the data was scaled -1 to 1 and the p-value for correct flux prediction was 0.1
 
 	+ the inputs were all of the columns besides the flux label.
@@ -24,4 +27,3 @@ Time-of documentation:
 	+ this is most likey not proper, as flux_err for an input might not be kosher
 
 	+ the accuracy on the training set was [correct guesses: 9475 / total guesses: 11107] ~ 0.8531 %
-	
--- demo.py
+++ demo.py
@@ -2,13 +2,38 @@ import scipy as sci
 import pandas as pd
 import numpy as np
 
-import torch
-import torchvision as tv
+# import torch
+# import torchvision as tv
+
+import matplotlib.pyplot as plt
 
 
-if __name__ == '__main__':
-	df = pd.read_csv('./data/test_set_sample.csv')
-	objects = [obj for obj in df.groupby('object_id')]
-	print(len(objects))
-	print(df.describe())
-	
+passbands = {0 : 'r',
+			 1 : 'g',
+			 2 : 'b',
+			 3 : 'c',
+			 4 : 'm',
+			 5 : 'y',} 
+
+df = pd.read_csv('./data/test_set_sample.csv')
+objects = [obj for obj in df.groupby(by='object_id', as_index=False)] 
+print(len(objects))
+print(df.describe())
+
+obj = objects[0]
+
+obj = obj[1]  # tuple -> df
+colors = []
+bands = obj['passband']
+
+for band in bands:
+	colors.append(passbands[band])
+
+plt_x = obj['mjd']
+plt_y = obj['flux']
+# want to color dots, according to passband
+# going thru every row is most likely inefficient.
+
+plt.scatter(plt_x, plt_y, c=colors, s=1)
+plt.show()
+# type(objects[0]) == tuple

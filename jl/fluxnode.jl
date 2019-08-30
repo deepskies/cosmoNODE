9dfa7ab163f2cf327e953f8249a014d378431ad7
anandijain <anandj@uchicago.edu>
2019-08-30 11:28:57 -0500

    module

--- jl/fluxnode.jl
+++ jl/fluxnode.jl
@@ -12,10 +12,12 @@ module FluxNode
     using DifferentialEquations, Flux, DiffEqFlux, CSV
 
     function fluxnode()
-	    df = CSV.read("../demos/data/training_set.csv")
-	    groups = groupby(df, [:object_id, :passband])
-		g = groups[1]
-
+	df = CSV.read("../demos/data/training_set.csv")
+	groups = groupby(df, [:object_id, :passband])
+	g = groups[1]
+	df_lc = g[:, [:
+	band_data = convert(Matrix, g)
+	
 	return
         # X, Y = FL.FluxLoader()
     end
